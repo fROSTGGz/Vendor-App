5826003c5a4fd582e6556c5e5b9198bdd910bc58
Keshav Chahal <keshavchahal10@gmail.com>
2025-01-18 21:22:21 +0530

    Cors added

---
+++ backend/middleware/corsMiddleware.js
@@ -0,0 +1,9 @@
+export const corsMiddleware = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+};
