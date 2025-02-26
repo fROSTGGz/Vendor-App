701e4a1562f4cf0c12d67ad548a74f4c3dd018c7
Keshav Chahal <keshavchahal10@gmail.com>
2025-02-26 19:16:21 +0530

    Cors error fixed pt2

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -1,5 +1,5 @@
 export const corsMiddleware = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://sristi-kheduthaat.vercel.app"); // Allow all origins
+  res.header("Access-Control-Allow-Origin", "https://vendor-app-8wrx.onrender.com"); // Allow the frontend origin
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
