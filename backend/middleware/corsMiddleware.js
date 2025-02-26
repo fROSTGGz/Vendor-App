b88b9ad0f39f6b71fb77dff9defea0a8ebc9c245
Keshav Chahal <keshavchahal10@gmail.com>
2025-02-26 19:29:11 +0530

    Cors error fixed pt4

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -10,6 +10,8 @@ export const corsMiddleware = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
+  } else {
+    console.log(`Origin ${origin} not allowed by CORS`);
   }
 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
